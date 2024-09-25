8860d5ba9d18bc7ca6976616d48a406551887267
styven <styvenn16@gmail.com>
2024-09-25 19:29:07 -0300

    adicionando o day.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 "use strict"
+
+//confuguração do dayjs
+import "./libs/dayjs.js"
+
+
 import "./styles/global.css"
 import "./styles/form.css"
 import "./styles/schedule.css"
+
+import dayjs from "dayjs"
+console.log(dayjs().format("HH:mm"))
