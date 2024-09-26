a5c29fa8860cba17ed621d3dbf9906f198a3f73a
styven <styvenn16@gmail.com>
2024-09-26 08:15:31 -0300

    deinindo a data autual

---
+++ src/modules/form/submit.js
@@ -0,0 +1,21 @@
+import dayjs from "dayjs"
+
+const form = document.querySelector("form")
+
+const colocarData = dayjs(new Date()).format("YYYY-MM-DD")
+
+//selecionar data atual
+const selectDate = document.getElementById("date")
+////
+//colocar a data atual
+selectDate.value = colocarData
+///////
+
+//defina date minima
+selectDate.min = colocarData
+/////
+
+form.onsubmit = (event) =>{
+
+    event.preventDefault()
+}
