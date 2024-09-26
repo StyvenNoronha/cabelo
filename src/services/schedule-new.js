d6af845ef41fa334fb1a063c2a411d2e7bd728a2
styven <styvenn16@gmail.com>
2024-09-26 13:58:46 -0300

    criando o serviço de agendamento

---
+++ src/services/schedule-new.js
@@ -0,0 +1,17 @@
+import {apiConfig} from "./api.config.js"
+
+export async function scheduleNew(id, nome, when) {
+    try {
+        await fetch (`${apiConfig}/schedules`,{
+            method:'POST',
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({id,nome,when})
+        })
+        alert("deu tudo certo.")
+    } catch (error) {
+        console.log(error)
+        alert("deu erro")
+    }
+}
