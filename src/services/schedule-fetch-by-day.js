5895434a9f1ce0ebb143660b66469005ed7dc0d5
styven <styvenn16@gmail.com>
2024-09-27 07:43:03 -0300

    Serviço para buscar agendamentos do dia

---
+++ src/services/schedule-fetch-by-day.js
@@ -0,0 +1,16 @@
+import {apiConfig} from "./api.config"
+import dayjs from "dayjs"
+export async function scheduleFetchByDay({date}) {
+    try {
+        //so fazendo uma busca
+        const reponse = await fetch(`${apiConfig.baseURL}/schedules`)
+        //converte para json
+        const data = await Response.json()
+        //filtra os agendamentos pelo dia selecionado
+        const dailySchedules = data.filter((schedule)=> dayjs(date).isSame(schedule.when, "day") )
+        return dailySchedules
+    } catch (error) {
+        console.log(error)
+        alert("deu erro do dia")
+    }
+}
